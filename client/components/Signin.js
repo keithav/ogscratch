24b20ed3110c709ec030eb5a6487a2676b1de48d
Sam Silver <samsilver@wustl.edu>
2019-05-02 10:28:19 -0700

    Thursday Morning Sync

--- client/components/Signin.js
+++ client/components/Signin.js
@@ -50,14 +50,18 @@ class Signin extends Component {
       <div id="mainContainer">
         <div id="loginCard">
           <h3>Please Login</h3>
-          <label htmlFor="loginUsername">Username</label>
-          <input id="loginUsername" type="text" onChange={(e) => this.props.loginUsername(e)} id="username" placeholder="username"></input>
-          <label htmlFor="loginPassword">Password</label>
-          <input id="loginUsername" type="password" onChange={(e) => this.props.loginPassword(e)} id="password" placeholder="password"></input>
-          <button id="loginButton" onClick={(e) => { e.preventDefault(); this.props.verifyLogin(this.props.username, this.props.password) }}>Login</button>
-          <br></br>
-          <br></br>
-          <button id="signupButton" onClick={(e) => { e.preventDefault(); this.props.signup() }}>Signup</button>
+          <div id="usernameContainer">
+            <label htmlFor="loginUsername">Username: </label>
+            <input id="loginUsername" type="text" onChange={(e) => this.props.loginUsername(e)} id="username" placeholder="username" required></input>
+          </div>
+          <div id="passwordContainer">
+            <label htmlFor="loginPassword">Password: </label>
+            <input id="loginPassword" type="password" onChange={(e) => this.props.loginPassword(e)} id="password" placeholder="password" required></input>
+          </div>
+          <div id="buttonContainer">
+            <button id="loginButton" onClick={(e) => { e.preventDefault(); this.props.verifyLogin(this.props.username, this.props.password) }}>Login</button>
+            <button id="signupButton" onClick={(e) => { e.preventDefault(); this.props.signup() }}>Signup</button>
+          </div>
         </div>
       </div>
     )
